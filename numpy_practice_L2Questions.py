162446e1a7652eb5aaa0cfeeee96d4e717d9367c
Archit Jain <60356590+Jain-Archit@users.noreply.github.com>
2020-02-29 17:56:08 +0530

    Add files via upload

--- numpy_practice_L2Questions.py
+++ numpy_practice_L2Questions.py
@@ -35,10 +35,10 @@ def A3():
     res = np.hstack((a,b))
     print(res)
 
-Q4 = " Create the following pattern without hardcoding. Use only numpy functions and the below input array a.
+Q4 = """ Create the following pattern without hardcoding. Use only numpy functions and the below input array a.
          a = np.array([1,2,3])
          output = array([1, 1, 1, 2, 2, 2, 3, 3, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3])
-     "
+     """
 def A4():
     a = np.arange(1,4,1)
     print(a)
@@ -48,8 +48,82 @@ def A4():
     print(c)
     d = np.r_[b,c]
     print(d)
-    """ np.r_ does what?"""    
+    """ np.r_ does what? Used for row-wise merging of two arrays"""    
     
-Q5 = 
+Q5 = """ Get the common items between a and b
+         a = np.array([1,2,3,2,3,4,3,4,5,6])
+         b = np.array([7,2,10,2,7,4,9,4,9,8])
+     """
 def A5():
+    a = np.array([1,2,3,2,3,4,3,4,5,6])
+    b = np.array([7,2,10,2,7,4,9,4,9,8])
+    c = np.intersect1d(a,b)
+    print(c)
     
+Q6 = """ From array a remove all items present in array b
+     a = np.array([1,2,3,4,5])
+     b = np.array([5,6,7,8,9])
+     """
+def A6():
+    a = np.array([1,2,3,4,5])
+    b = np.array([3,5,6,7,8,9])
+    c = np.setdiff1d(a,b)
+    print(c)
+    
+Q7 = """ Get the positions where elements of a and b match
+     a = np.array([1,2,3,2,3,4,3,4,5,6])
+     b = np.array([7,2,10,2,7,4,9,4,9,8])
+     """
+def A7():
+    a = np.array([1,2,3,2,3,4,3,4,5,6])
+    b = np.array([7,2,10,2,7,4,9,4,9,8])
+    idx = np.where(a == b)
+    print(idx)
+
+Q8 = """ Get all items between 5 and 10 from a.
+     a = np.array([2, 6, 1, 9, 10, 3, 27])
+     """
+def A8():
+    a = np.array([2, 6, 1, 9, 10, 3, 27])
+    idx1 = a[a>=5]
+    idx2 = a[a<=10]
+    res = np.intersect1d(idx1,idx2)
+    print(res)
+    """ Alternative Solution
+        index = np.where(np.logical_and(a>=5, a<=10))
+        a[index]
+    """
+
+Q9 = """ Convert the function maxx that works on two scalars, to work on two arrays.
+         def maxx(x, y):
+            if x >= y:
+                return x
+            else:
+                return y
+        
+            maxx(1, 5)
+            #> 5
+            
+         a = np.array([5, 7, 9, 8, 6, 4, 5])
+         b = np.array([6, 3, 4, 8, 9, 7, 1])
+         pair_max(a, b)
+         #> array([ 6.,  7.,  9.,  8.,  9.,  7.,  5.])
+      """
+def A9():
+    def maxx(x,y):
+        if x >= y:
+            return x
+        else:
+            return y
+        
+    pair_max = np.vectorize(maxx, otypes=[float])
+    a = np.array([5, 7, 9, 8, 6, 4, 5])
+    b = np.array([6, 3, 4, 8, 9, 7, 1])
+    c = pair_max(a, b)
+    print(c)
+    """
+        np.vectorize : 
+    """
+
+Q10 = 
+
